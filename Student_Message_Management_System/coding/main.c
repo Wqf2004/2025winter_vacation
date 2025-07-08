bc72d1ab1ec4aab3ad1c11440a7e8df496525151
peakWen <2042605863@qq.com>
2025-07-08 12:24:20 +0800

    make it more available

--- Student_Message_Management_System/coding/main.c
+++ Student_Message_Management_System/coding/main.c
@@ -6,7 +6,7 @@
 #define MAX_STUDENTS 30
 #define MAX_Grades 100
 #define FILENAME_A "../dataset/a.txt" // 学生信息存储地址
-#define FILENAME_B "../datasetb.txt"  // 成绩信息存储地址
+#define FILENAME_B "../dataset/b.txt"  // 成绩信息存储地址
 
 // 学生信息结构体
 typedef struct student
@@ -23,7 +23,7 @@ typedef struct grades
 {
     char id[10];         // 学生学号
     char courseId[20];   // 课程代码
-    char courseName[50]; // 课程名字
+    char courseName[80]; // 课程名字
     float credits;         // 学分
     float usualScore;    // 平时成绩
     float labScore;      // 实验成绩
@@ -32,7 +32,9 @@ typedef struct grades
 } Grades;
 
 int readStudentsFromFile(char *FileName, Student *pStu);
+void printStudent();
 int checkStudentId(Student students[], int count, const char *id);
+char* returnStudentName(Student students[], int count, const char *id);
 float calculateTotalScore(float usual, float lab, float exam);
 float calculateCredits(float credit, float totalScore);
 void addGradesRecord(char *FileName_A, char* FileName_B, Student *student);
@@ -93,10 +95,9 @@ int main()
 
                     switch(choice){
                         case '1':
-                            printf("请输入你最大可能录入的成绩数量(推荐值:5)：\n");
-                            int Lcount;
-                            scanf(" %d", &Lcount);
-                            Student *pStu = (Student *)malloc(Lcount * sizeof(Student));
+                        {  // case标签后面的语句也需要放到花括号下
+                            printStudent();
+                            Student *pStu = (Student *)malloc(MAX_Grades * sizeof(Student));
                             if (pStu == NULL) {
                                 printf("内存分配竟然失败！！\n");
                                 goto MAIN_MENU; // 强制返回上一级
@@ -104,10 +105,12 @@ int main()
                             addGradesRecord(FILENAME_A, FILENAME_B, pStu); // 录入学生成绩
                             free(pStu); // 释放内存
                             break;
+                        }
                         case '0':
                             goto MAIN_MENU;
                         default:
                             printf("无效选择，请重新输入\n");
+                            getchar();getchar();getchar();
                     }
                 }
             case 'A':
@@ -127,6 +130,7 @@ int main()
                             goto MAIN_MENU;
                         default:
                             printf("无效选择，请重新输入\n");
+                            getchar();getchar();getchar();
                     }
                 }
                 break;
@@ -145,6 +149,7 @@ int main()
                             goto MAIN_MENU;
                         default:
                             printf("无效选择，请重新输入\n");
+                            getchar();getchar();getchar();
                     }
                 }
                 break;
@@ -156,6 +161,7 @@ int main()
                 
             default:
                 printf("无效选择，请重新输入\n");
+                getchar();getchar();getchar();
         }
     }
     
@@ -172,27 +178,34 @@ int readStudentsFromFile(char* FileName, Student* pStu)
 		printf("The file is error.\n");
 		exit(0);
 	}
-
+    
 	/*跳过表头信息*/ 
 	while(fgetc(fp) != '\n'); // 跳过第一行
-
+    
 	/*读入到学生数组中*/
 	int i = 0;
 	while (fscanf(fp, "%s %s %s %s %s\n", pStu[i].id, pStu[i].name, pStu[i].sex, pStu[i].room, pStu[i].phone) == 5) 
 	{
-
 		i++; // 每读取一名学生的信息计数加一
 	}
+    printf("学生的学号及姓名信息：\n");
 	fclose(fp);
 
-	/*打印出学生的信息*/
 	int sum = i;
-	for (i = 0; i < sum; i++)
-	{
-		printf("%s %s %s %s %s\n", pStu[i].id, pStu[i].name, pStu[i].sex, pStu[i].room, pStu[i].phone);
-	}
-
 	return sum;
+}
+
+/*打印出学生的信息*/
+void printStudent()
+{
+    // 读取学生数据
+    Student students[MAX_STUDENTS];
+    int sum = readStudentsFromFile(FILENAME_A, students);
+    int i;
+    for (i = 0; i < sum; i++)
+	{
+		printf("%s %s\t", students[i].id, students[i].name);
+	}
 }
 
 // 检查学号信息是否存在于学生信息表中
@@ -204,6 +217,17 @@ int checkStudentId(Student students[], int count, const char *id)
         }
     }
     return 0; // 不存在
+}
+
+// 根据学号信息返回学生姓名
+char* returnStudentName(Student students[], int count, const char *id)
+{
+    for (int i = 0; i < count; i++) {
+        if (strcmp(students[i].id, id) == 0) {
+            return students[i].name; 
+        }
+    }
+    return ""; 
 }
 
 // 计算综合成绩
@@ -237,7 +261,7 @@ float calculateCredits(float credit, float totalScore) {
 void addGradesRecord(char *FileName_A, char* FileName_B, Student *student) 
 {
     int studentCount = readStudentsFromFile(FileName_A, student);
-    
+    printf("\n");
     if (studentCount == 0) {
         printf("没有学生信息，请先在 %s 中添加学生数据\n", FileName_A);
         return;
@@ -257,11 +281,12 @@ void addGradesRecord(char *FileName_A, char* FileName_B, Student *student)
     fseek(file, 0, SEEK_END);
 
     if (ftell(file) == 0) {
-        fprintf(file, "学号 课程编号 课程名称 学分 平时成绩 实验成绩 卷面成绩 综合成绩\n");
-        fseek(file, 0, SEEK_END);
+        fprintf(file, "学号 课程编号 课程名称 学分 平时成绩 实验成绩 卷面成绩 综合成绩");
+        fprintf(file, "\n");
     }
 
     while (1) {
+        fseek(file, 0, SEEK_END);
         printf("请输入学号（输入0退出）：");
         scanf("%s", id);
         
@@ -293,9 +318,8 @@ void addGradesRecord(char *FileName_A, char* FileName_B, Student *student)
         printf("请输入卷面成绩：");
         scanf("%f", &grades.examScore);
         
-        // 璁＄畻缁煎悎鎴愮哗
+        // 计算总评成绩
         grades.totalScore = calculateTotalScore(grades.usualScore, grades.labScore, grades.examScore);
-        
         
         if (file == NULL) {
             printf("无法打开文件 %s\n", FileName_B);
@@ -307,9 +331,9 @@ void addGradesRecord(char *FileName_A, char* FileName_B, Student *student)
                 calculateCredits(grades.credits,grades.totalScore), grades.usualScore, grades.labScore,
                 grades.examScore, grades.totalScore);
         
-        fclose(file);
         printf("成绩记录添加成功！\n");
     }
+    fclose(file);
 }
 
 int readGradesFromFile(char *FileName, Grades *pGrd)
@@ -368,7 +392,6 @@ void queryStudentSmart()
                 printf("性别：%s\n", students[i].sex);
                 printf("宿舍号：%s\n", students[i].room);
                 printf("电话号码：%s\n", students[i].phone);
-                printf("------------------------\n");
                 found = 1;
                 break;
             }
@@ -383,7 +406,6 @@ void queryStudentSmart()
                 printf("性别：%s\n", students[i].sex);
                 printf("宿舍号：%s\n", students[i].room);
                 printf("电话号码：%s\n", students[i].phone);
-                printf("------------------------\n");
                 found = 1;
                 break;
             }
@@ -406,12 +428,12 @@ void queryStudentsByDorm()
     int found = 0;
     for (int i = 0; i < studentCount; i++) {
         if (strcmp(students[i].room, dorm) == 0) {
+            printf("------------------------\n");
             printf("学号：%s\n", students[i].id);
             printf("姓名：%s\n", students[i].name);
             printf("性别：%s\n", students[i].sex);
             printf("宿舍号：%s\n", students[i].room);
             printf("电话号码：%s\n", students[i].phone);
-            printf("------------------------\n");
             found = 1;
         }
     }
@@ -431,24 +453,32 @@ void queryScoresById()
     // 读取成绩数据
     Grades scores[MAX_Grades];
     int scoreCount = readGradesFromFile(FILENAME_B, scores);
+    // 读取学生信息
+    Student students[MAX_STUDENTS];
+    int studentCount = readStudentsFromFile(FILENAME_A, students);
+    char *name = returnStudentName(students, studentCount, id);
 
     // 查询并显示成绩
-    int scoreFound = 0;
+    int found = 0;
+    float allCredit = 0;
     for (int i = 0; i < scoreCount; i++) {
         if (strcmp(scores[i].id, id) == 0) {
-            printf("课程编号：%s\n", scores[i].courseId);
-            printf("课程名称：%s\n", scores[i].courseName);
-            printf("学分：%.1f\n", scores[i].credits);
-            printf("平时成绩：%.1f\n", scores[i].usualScore);
-            printf("实验成绩：%.1f\n", scores[i].labScore);
-            printf("卷面成绩：%.1f\n", scores[i].examScore);
-            printf("综合成绩：%.1f\n", scores[i].totalScore);
+            printf("学号：%s ", id);
+            printf("姓名：%s ", name);
+            printf("课程编号：%s ", scores[i].courseId);
+            printf("课程名称：%s ", scores[i].courseName);
+            printf("综合成绩：%.1f ", scores[i].totalScore);
+            printf("实得学分：%.2f\n", scores[i].credits);
             printf("------------------------\n");
-            scoreFound = 1;
+            found++;
+            allCredit = allCredit + scores[i].credits;
         }
     }
     
-    if (!scoreFound) {
+    if (found) {
+        printf("共修：%d科，实得总学分为：%.2f", found, allCredit);
+    }
+    else{
         printf("未找到该学生的成绩记录\n");
     }
 }
